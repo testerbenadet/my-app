f155e6ad6cd831c1104e3ddb5c598e6003cbaa37
testerbenadet <nicolausbenadet@hotmail.com>
2024-11-04 13:34:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ export default function App() {
     gb.setAttributes({
       id: user.id,
       company: user.company,
+      anonymous_id: userPseudoId
     });
   }, []); // Empty dependency array to run once on mount
 
